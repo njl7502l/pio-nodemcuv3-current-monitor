5b7d2beb8d6685e4cff32113c7904adc9d58b60f
NJL7502L <g.hana1211.tako@gmail.com>
2021-02-14 12:44:11 +0900

    単純移動平均を追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,48 @@
 #include "arduino.h"
+#include <Ticker.h>
+#include <queue>
 
-void setup() { pinMode(2, OUTPUT); }
+Ticker ticker;
+
+class SimpleMovingAverage {
+private:
+  std::queue<int> voltageQueue;
+  int voltageQueueSize = 100;
+  int averageSum = 0;
+
+public:
+  void setVoltageQueueSize(int size) { voltageQueueSize = size; }
+
+  void push(int val) {
+    voltageQueue.push(val);
+    averageSum += val;
+
+    if (voltageQueue.size() > voltageQueueSize) {
+      averageSum -= voltageQueue.front();
+      voltageQueue.pop();
+    }
+  }
+
+  float cal(void) { return averageSum / (float)voltageQueue.size(); }
+} sma;
+
+void flash() { sma.push(analogRead(A0)); }
+
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+
+  ticker.attach_ms(100, flash); // 100ms,(10Hz)ごとに読み取り
+  sma.setVoltageQueueSize(100); // 10秒前の値まで影響を受けるキューのサイズ
+}
 
 void loop() {
   digitalWrite(2, HIGH);
   delay(500);
   digitalWrite(2, LOW);
   delay(500);
+
+  Serial.print(sma.cal());
+  Serial.println();
 }
